77208b75fad4183974789516aa6e0de3155ddbbe
Somdipto Chakraborty <108862434+trap-representation@users.noreply.github.com>
2023-07-18 17:16:52 +0530

    Cleaning up

--- docs/navbar.js
+++
@@ -1,7 +0,0 @@
-
-        let btn = document.querySelector('#btn');
-        let dropBar = document.querySelector('.drop-bar');
-
-        btn.onclick = function(){
-            dropBar.classList.toggle('active');
-        }
